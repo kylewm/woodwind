4a59ca58c7d30ee1ae7d2769072fb02f774a5a82
Kyle Mahan <kyle.mahan@gmail.com>
2015-02-02 08:50:56 -0800

    bump version of Flask-Micropub

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+from fabric.api import local, prefix, cd, run, env, lcd
+
+env.hosts = ['orin.kylewm.com']
+
+REMOTE_PATH = '/srv/www/kylewm.com/woodwind'
+
+
+def commit():
+    local("git add -p")
+    local("git diff-index --quiet HEAD || git commit")
+
+
+def push():
+    local("git push origin master")
+
+
+def pull():
+    with cd(REMOTE_PATH):
+        run("git pull origin master")
+        run("git submodule update")
+
+
+def restart():
+    with cd(REMOTE_PATH):
+        with prefix("source venv/bin/activate"):
+            run("pip install -r requirements.txt")
+            # run("uwsgi --reload /tmp/redwind.pid")
+            run("supervisorctl restart woodwind woodwind-celery")
+
+
+def deploy():
+    commit()
+    push()
+    pull()
+    restart()
