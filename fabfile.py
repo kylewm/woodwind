20b757c31564be88a4e574661af72cbf57487858
Kyle Mahan <kyle.mahan@gmail.com>
2015-03-14 20:02:47 +0000

    remove reference to celery in fabric file

--- fabfile.py
+++ fabfile.py
@@ -32,7 +32,6 @@ def restart():
         with prefix("source venv/bin/activate"):
             run("pip install --upgrade -r requirements.txt")
     sudo("restart woodwind")
-    sudo("restart woodwind-celery")
     sudo("restart woodwind-tornado")
 
 
