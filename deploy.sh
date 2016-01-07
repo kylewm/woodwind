b49c53eeda1e4bccaf87ba32496fdeca8fa437f5
Kyle Mahan <kyle.mahan@gmail.com>
2016-01-07 11:53:55 -0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REMOTE_USER=kmahan
+HOST=orin.kylewm.com
+REMOTE_PATH=/srv/www/kylewm.com/woodwind
+
+ssh $REMOTE_USER@$HOST bash -c "'
+
+set -x
+cd $REMOTE_PATH
+
+git pull origin master \
+&& source venv/bin/activate \
+&& pip install --upgrade -r requirements.txt \
+&& sudo restart silopub
+
+'"
