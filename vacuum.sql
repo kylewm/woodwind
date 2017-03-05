28c1ab50589a11ac73e0a744e91342ed22c59832
Kyle Mahan <kyle.mahan@medium.com>
2017-03-05 15:05:46 -0800

    simplify vacuum script a little

--- vacuum.sql
+++ vacuum.sql
@@ -2,9 +2,8 @@ DELETE FROM entry
 USING (
   SELECT
     id,
-    retrieved,
     ROW_NUMBER() OVER (PARTITION BY feed_id ORDER BY retrieved DESC) AS row
   FROM entry
 ) AS numbered
 WHERE entry.id = numbered.id
-  AND (numbered.row > 2000 OR numbered.retrieved < CURRENT_DATE - INTERVAL '365 days')
+  AND (row > 2000 OR retrieved < CURRENT_DATE - INTERVAL '365 days')
